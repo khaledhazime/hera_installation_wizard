63c9d7a2be02ac9a3b4ed3a8730fc18152e30a82
Khaled Hazime <hazimekhaled@icloud.com>
2024-01-04 20:33:01 -0300

    Refactor Sidebar.py: Improve code formatting and readability

--- src/ui/Sidebar.py
+++ src/ui/Sidebar.py
@@ -3,11 +3,14 @@ from PIL import Image, ImageTk
 from tkinter import Tk
 from ui.Colors import Colors
 
+
 class Sidebar(ctk.CTkFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs, corner_radius=0)
         self.colors = Colors()
-        self.pack(side="left", fill="y", padx=0, pady=0)  # ipadx sets the internal padding, adjusting the width
+        self.pack(
+            side="left", fill="y", padx=0, pady=0
+        )  # ipadx sets the internal padding, adjusting the width
 
         self.configure(fg_color=self.colors.palette["sidebar_background"])
         self.pack_propagate(0)
@@ -24,11 +27,23 @@ class Sidebar(ctk.CTkFrame):
         new_height = min(height, int(width / aspect_ratio))
         image = image.resize((new_width, new_height), Image.ANTIALIAS)
         self.image = ImageTk.PhotoImage(image)
-        image_label = ctk.CTkLabel(self, image=self.image, width=new_width, height=new_height, text="")
+        image_label = ctk.CTkLabel(
+            self, image=self.image, width=new_width, height=new_height, text=""
+        )
         image_label.pack(pady=10)
 
     def add_button(self, text):
-        button = ctk.CTkButton(self, text=text, state="disabled", fg_color="gray", hover_color="gray", text_color_disabled="black", width=180, height=30, font=("Futura", 12, "bold"))
+        button = ctk.CTkButton(
+            self,
+            text=text,
+            state="disabled",
+            fg_color="gray",
+            hover_color="gray",
+            text_color_disabled="black",
+            width=180,
+            height=30,
+            font=("Futura", 12, "bold"),
+        )
         button.pack(pady=1)
         self.buttons.append(button)
         return button
@@ -44,6 +59,16 @@ class Sidebar(ctk.CTkFrame):
     def set_button_pressed(self, index):
         for idx, button in enumerate(self.buttons):
             if idx == index:
-                button.configure(fg_color=self.colors.palette["active_button_color"], hover_color=self.colors.palette["active_button_color"], text_color_disabled="black", font=("Futura", 12, "bold"))
+                button.configure(
+                    fg_color=self.colors.palette["active_button_color"],
+                    hover_color=self.colors.palette["active_button_color"],
+                    text_color_disabled="black",
+                    font=("Futura", 12, "bold"),
+                )
             else:
-                button.configure(fg_color=self.colors.palette["inactive_button_color"], hover_color=self.colors.palette["inactive_button_color"], text_color_disabled="black", font=("Futura", 12, "bold"))
+                button.configure(
+                    fg_color=self.colors.palette["inactive_button_color"],
+                    hover_color=self.colors.palette["inactive_button_color"],
+                    text_color_disabled="black",
+                    font=("Futura", 12, "bold"),
+                )

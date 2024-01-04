dfee70713a28cca3508764e79002af17fa1de754
Khaled Hazime <hazimekhaled@icloud.com>
2024-01-04 20:30:10 -0300

    Update color palette in Colors.py

--- src/ui/Colors.py
+++ src/ui/Colors.py
@@ -1,9 +1,10 @@
 class Colors:
     def __init__(self):
         self.palette = {
-            'main_background': '#EFEFEF',
-            'sidebar_background': '#ADD7F6',
-            'titlebar_background': '#FFFFFF',
-            'inactive_button_color': '#87BFFF',
-            'active_button_color': '#2667FF',
+            'main_background': '#CAF0F8',
+            'sidebar_background': '#90E0EF',
+            'titlebar_background': '#90E0EF',
+            'inactive_button_color': '#48CAE4',
+            'active_button_color': '#00B4D8',
+            'text_color': 'black',
         }

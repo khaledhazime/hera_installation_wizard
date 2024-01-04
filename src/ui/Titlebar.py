24baefee18c4e76e978158f4424b18dbe4ab0a05
Khaled Hazime <hazimekhaled@icloud.com>
2024-01-04 20:31:29 -0300

    Refactor Titlebar widget

--- src/ui/Titlebar.py
+++ src/ui/Titlebar.py
@@ -1,11 +1,14 @@
 import customtkinter as ctk
 from ui.Colors import Colors
 
+
 class Titlebar(ctk.CTkFrame):
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs, corner_radius=0)
         self.pack(side="top", fill="x", pady=0, padx=0)
         self.configure(fg_color=Colors().palette["titlebar_background"])
 
-        self.title_label = ctk.CTkLabel(self, text="HERA Wizard", text_color = "black", font=("Futura", 20, "bold"))
-        self.title_label.pack(side="left", fill="x", padx=10, pady=10)
+        self.title_label = ctk.CTkLabel(
+            self, text="HERA Wizard", text_color="black", font=("Futura", 20, "bold")
+        )
+        self.title_label.pack(fill="x", padx=10, pady=10)

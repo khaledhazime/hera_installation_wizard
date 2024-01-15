8fbde257d8ccafff7848fe894be39e0b1827c2a7
Khaled Hazime <hazimekhaled@icloud.com>
2024-01-15 11:15:42 -0300

    Refactor Footer class in Footer.py

--- src/ui/Footer.py
+++ src/ui/Footer.py
@@ -1,9 +1,8 @@
 import customtkinter as ctk
 from ui.Colors import Colors
 
-
 class Footer(ctk.CTkFrame):
-    def __init__(self, parent, *args, **kwargs):
+    def __init__(self, parent, ui_reference, *args, **kwargs):
         super().__init__(parent, *args, **kwargs, corner_radius=0)
         self.pack(side="bottom", fill="x", pady=0, padx=0)
         self.configure(fg_color=Colors().palette["titlebar_background"])
@@ -14,9 +13,10 @@ class Footer(ctk.CTkFrame):
             fg_color=Colors().palette["inactive_button_color"],
             hover_color=Colors().palette["active_button_color"],
             font=("Futura", 12, "bold"),
-            width=180,
+            width=120,
             height=30,
             text_color="black",
+            command=self.on_cancel  # Assuming a cancel button functionality
         )
         self.cancel_button.pack(side="left", fill="x", padx=10, pady=10)
 
@@ -26,20 +26,28 @@ class Footer(ctk.CTkFrame):
             fg_color=Colors().palette["inactive_button_color"],
             hover_color=Colors().palette["active_button_color"],
             font=("Futura", 12, "bold"),
-            width=180,
+            width=120,
             height=30,
             text_color="black",
+            # Replace with the actual screen you want to switch to
+            command=lambda: ui_reference.change_screen("welcome")
         )
         self.back_button.pack(side="left", fill="x", padx=10, pady=10)
-        
+
         self.next_button = ctk.CTkButton(
             self,
             text="Next",
             fg_color=Colors().palette["inactive_button_color"],
             hover_color=Colors().palette["active_button_color"],
             font=("Futura", 12, "bold"),
-            width=180,
+            width=120,
             height=30,
             text_color="black",
+            # Replace with the actual screen you want to switch to
+            command=lambda: ui_reference.change_screen("github")
         )
         self.next_button.pack(side="left", fill="x", padx=10, pady=10)
+
+    def on_cancel(self):
+        # Define what should happen when Cancel is pressed
+        pass
